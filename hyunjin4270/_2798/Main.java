ff03b2808ce629e6918092d7ab4f398f4109e8a0
hyunjin4270 <hyunn4270@gmail.com>
2024-05-16 17:49:25 +0900

    solve backjoon

---
+++ hyunjin4270/_2798/Main.java
@@ -0,0 +1,35 @@
+package _2798;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] card = new int[Integer.parseInt(st.nextToken())]; //카드 개수
+        int limit = Integer.parseInt(st.nextToken()); //수 리미트 지정
+        int maxScore = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < card.length; i++) { // 카드 정수변수 안에 숫자 집어넣기
+            card[i] = Integer.parseInt(st.nextToken());
+        }
+
+        //모든 경우의수 구하기
+        for (int i = 0, score; i < (card.length - 2); i++) { //첫번째 카드 지정
+            for (int j = (i + 1); j < (card.length - 1); j++) { //두번째 카드 지정
+                for (int k = (j + 1); k < card.length; k++) { //세번째 카드 지정
+                    int i1 = card[i] + card[j] + card[k];
+                    if (i1 <= limit) {
+                        score = i1;
+                        maxScore = Math.max(maxScore, score);
+                    }
+                }
+            }
+        }
+        System.out.println(maxScore);
+    }
+
+
+}
