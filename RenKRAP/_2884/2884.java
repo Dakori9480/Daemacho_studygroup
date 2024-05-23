a92b24d129b5bf7487dd6ba30b4a5ad09b73d7d6
RenK_RAP <1005woojin@gmail.com>
2024-05-23 12:08:47 +0900

    Resolve 2884
    알람시계

---
+++ RenKRAP/_2884/2884.java
@@ -0,0 +1,25 @@
+알람 맞추기
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int userInputH= sc.nextInt();
+        int userInputM = sc.nextInt();
+
+        if (userInputM < 45) {
+            if (userInputH == 0){
+                userInputH = 23;
+            } else {
+                userInputH = userInputH - 1;
+            }
+            userInputM = 15 + userInputM;
+        } else {
+            userInputM = userInputM - 45;
+        }
+
+        System.out.printf("%d %d", userInputH, userInputM);
+
+    }
+}
