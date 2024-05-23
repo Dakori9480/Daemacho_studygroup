e1f7bd9ea37410005ee553ee75d0524f1b60e2ab
RenK_RAP <1005woojin@gmail.com>
2024-05-23 12:07:22 +0900

    Create 14681.java

---
+++ RenKRAP/_14681/14681.java
@@ -0,0 +1,24 @@
+사분면 구하기
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int userInputX= sc.nextInt();
+        int userInputY = sc.nextInt();
+
+        if (userInputX > 0 && userInputY >0) {
+            System.out.println(1);
+        }
+        if (userInputX < 0 && userInputY >0) {
+            System.out.println(2);
+        }
+        if (userInputX < 0 && userInputY < 0) {
+            System.out.println(3);
+        }
+        if (userInputX > 0 && userInputY < 0) {
+            System.out.println(4);
+        }
+    }
+}
